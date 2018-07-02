6d36ce97de2cf0dde994c4a9552127d9af643e4d
stefaniiik <stefanikrautstrunk@gmail.com>
2018-07-02 10:38:59 -0700

    initial set up and content added

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// function to get from the landing page to portfolio
+var welcomeButton = $('#welcome').on('click', function () {
+    console.log("welcomeButton");
+    $(this).parent().hide();
+    $('.container').show();
+    mainPage();
+});
+
+// function for displaying the results page
+var mainPage = function () {
+    window.location = "../portfolio/about.html";
+
+}
